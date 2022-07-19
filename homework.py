6a5cf92c6d3e92d773f5cae7ab520202c57e4401
Оля <olyakorcha@gmail.com>
2022-07-19 21:34:02 +0300

    my_project

--- homework.py
+++ homework.py
@@ -106,8 +106,8 @@ class Swimming(Training):
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
-        return (self.length_pool * self.count_pool / self.M_IN_KM
-               / self.duration)
+        return (self.length_pool * self.count_pool
+                / self.M_IN_KM / self.duration)
 
     def get_spent_calories(self) -> float:
         return ((self.get_mean_speed() + self.coeff_cal_1)
